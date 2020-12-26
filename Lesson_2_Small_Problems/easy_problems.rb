495a7b1222496f792df6b2291cfbbb3e3874899e
Haley Rairez <ramirezhaley@gmail.com>
2020-12-25 19:57:09 -0500

    began work on RB 101 easy problems

---
+++ Lesson_2_Small_Problems/easy_problems.rb
@@ -0,0 +1,31 @@
+# Repeater yourself
+def repeat (str, num)
+  num.times{ puts str}
+end
+
+repeat('Hello', 3)
+
+# Odd
+def is_odd?(num)
+  num % 2 == 1
+end
+
+puts is_odd?(2)    # => false
+puts is_odd?(5)    # => true
+puts is_odd?(-17)  # => true
+puts is_odd?(-8)   # => false
+puts is_odd?(0)    # => false
+puts is_odd?(7)    # => true
+
+# List of Digits
+def digit_list(num)
+  str = num.to_s
+  nums = str.split(//)
+  nums.map{|char| char.to_i}
+end
+
+
+puts digit_list(12345) == [1, 2, 3, 4, 5]     # => true
+puts digit_list(7) == [7]                     # => true
+puts digit_list(375290) == [3, 7, 5, 2, 9, 0] # => true
+puts digit_list(444) == [4, 4, 4]             # => true
