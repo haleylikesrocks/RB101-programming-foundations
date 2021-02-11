418694c9c2745c4345e229076a4e03c06965f260
Haley Rairez <ramirezhaley@gmail.com>
2021-02-10 20:02:44 -0600

    finished the seeing stars problem

--- Exercises/advanced_1.rb
+++ Exercises/advanced_1.rb
@@ -3,6 +3,26 @@
 
 # 2 Seeing Stars
 
+def star(num)
+  in_counter = num / 3
+  out_counter = 0
+
+  (num/2).times do
+    puts  ' ' * out_counter + '*' + ' ' * in_counter + '*' + ' ' * in_counter + '*'
+    in_counter -= 1
+    out_counter += 1
+  end
+  puts '*' * num
+  (num/2).times do
+    out_counter -= 1
+    in_counter += 1
+    puts  ' ' * out_counter + '*' + ' ' * in_counter + '*' + ' ' * in_counter + '*'
+  end
+end
+
+star(7)
+star(9)
+
 
 # 3 Transpose 3x3
 
