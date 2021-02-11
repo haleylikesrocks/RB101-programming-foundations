d6b3d91d2a4e5f9dc3df9165fdd571b59a29e858
Haley Rairez <ramirezhaley@gmail.com>
2021-02-10 19:47:37 -0600

    list out the problems

---
+++ Exercises/advanced_1.rb
@@ -0,0 +1,22 @@
+# 1 Madlibs Revisited
+
+
+# 2 Seeing Stars
+
+
+# 3 Transpose 3x3
+
+
+# 4 Transpose MxN
+
+
+# 5 Rotating Matrices
+
+# 6 Fix the Bug
+
+
+# 7 Merged Sorted Lists
+
+# 8 Merge Sort
+
+# 9 Eyptain Franctions
